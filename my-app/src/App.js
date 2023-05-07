a7f9a84bf441aa32a64688cb495a578a956ad3a8
Karasp1980 <karasp80@gmail.com>
2023-05-07 15:02:25 +0000

    add content.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,44 @@
-import css from './App.module.css';
+//import "./App.module.css";
+//import StatefulGreetingWithPrevState from "./components/StatefulGreetingWIthPrevState";
+//import NavBarSimple from "./components/NavBarSimple";
+//import Sidebar from "./components/Sidebar";
+//import EventsClass from "./components/EventsClass";
+//import EventsFunctional from "./components/EventsFunctional";
+//import ConditionalRenderingClass from "./components/ConditionalRenderingClass";
+//import ConditionalRenderingFunctional from "./components/ConditionalRenderingFunctional";
+//import NestingComponents from "./components/NestingComponents";
+//import MethodsAsPropsParent from "./components/MethodsAsPropsParent";
+//import NavBarForm from "./components/NavBarForm";
+//import css from './App.module.css';
+//import Content from "./components/Content";
+//import Clock from "./components/Clock";
+//import "./App.css";
+//import LifeCyclesCDM from "./components/LifeCyclesCDM";
+//import LifeCyclesCDU from "./components/LifeCyclesCDU";
+//import LifeCyclesCWU from "./components/LifeCyclesCWU";
+//import ControlledForm from "./components/ControlledForm";
+//import UncontrolledForm from "./components/UncontrolledForm";
+//import { SearchBar1 } from "./components/SearchBar1";
+//import { SearchBar2 } from "./components/SearchBar2";
+//import { SearchBar3 } from "./components/SearchBar3";
+
+import css from "./App.module.css";
+// import NavBarSimple from "./components/NavBarSimple";
+import Sidebar from "./components/Sidebar";
+import NavBarForm from "./components/NavBarForm";
+import Content from "./components/Content";
 
 function App() {
   return (
     <div className={css.App}>
       {/* Add your components here */}
+      <NavBarForm />
+      <Sidebar />
+      <Content />
     </div>
   );
 }
 
 export default App;
+
+
---
+++ my-app/src/components/E27Snippet.js
@@ -0,0 +1,62 @@
+#rfce
+import React from 'react'
+
+function E27Snippet() {
+  return (
+    <div>E27Snippet</div>
+  )
+}
+
+export default E27Snippet
+
+#rafca
+
+import React from 'react'
+
+const E27Snippet = () => {
+  return (
+    <div>E27Snippet</div>
+  )
+}
+
+export default E27Snippet
+
+import React, { Component } from 'react'
+
+export class E27Snippet extends Component {
+  render() {
+    return (
+      <div>E27Snippet</div>
+    )
+  }
+}
+
+export default E27Snippet
+
+#rce
+import React, { Component } from 'react'
+
+export class E27Snippet extends Component {
+  render() {
+    return (
+      <div>E27Snippet</div>
+    )
+  }
+}
+
+export default E27Snippet
+
+#rconst
+constructor(props) {
+  super(props)
+
+  this.state = {
+     first
+  }
+}
+
+#clg
+console.log(first)
+
+#clo
+console.log('first', first)
